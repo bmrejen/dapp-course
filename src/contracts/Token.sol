03251e306b04b507a66335fe766e63f0a9e834c9
Benoit Mrejen <benoit.mrejen@gmail.com>
2020-11-23 06:37:03 +0100

    add transfer function

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,12 +1,30 @@
 pragma solidity >=0.4.22 <0.8.0;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 contract Token {
+    using SafeMath for uint;
+
     string public name = "BMR Token";
     string public symbol = "BMR";
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
     constructor() public {
         totalSupply = 1000000 * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require(_to != address(0x0));
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,14 +1,15 @@
-const { before, iteratee } = require('lodash');
-const { contracts_build_directory } = require('../truffle-config');
+import { expect } from 'chai';
+import {tokens, checkNumber, EVM_REVERT} from './helpers';
 
 const Token = artifacts.require('Token');
 require('chai')
     .use(require('chai-as-promised'))
     .should();
 
-// args: name of contract, accounts
+// truffle provides contract(name of contract, account list)
 contract('Token', (accounts) => {
     let token;
+    const [deployer, receiver] = accounts;
     beforeEach(async() => {
         // fetch token from the blockchain
         token = await Token.new();
@@ -19,26 +20,75 @@ contract('Token', (accounts) => {
             const result = await token.name();
             result.should.equal('BMR Token');
         });
-    });
 
-    describe('deployment', () => {
         it('tracks the symbol', async () => {
             const result = await token.symbol();
             result.should.equal('BMR');
         });
-    });
 
-    describe('deployment', () => {
         it('tracks the decimals', async () => {
             const result = await token.decimals();
             result.toString().should.equal('18');
         });
-    });
 
-    describe('deployment', () => {
         it('tracks the total supply', async () => {
             const result = await token.totalSupply();
             result.toString().should.equal('1000000000000000000000000');
         });
+
+        it('deployer has all supply', async () => {
+            const result = await token.balanceOf(accounts[0]);
+            const totalSupply = await token.totalSupply();
+            result.toString().should.equal(totalSupply.toString());
+        });
+    });
+
+    describe('sending tokens', () => {
+        let amount;
+
+        describe('success', () => {
+            let result;
+            beforeEach('send amount', async () => {
+                amount = tokens(100000);
+                result = await token.transfer(receiver, amount, {from: deployer});
+            });
+            
+            it('transfer token balances', async () => {
+                const balanceOfDeployer = await token.balanceOf(deployer);
+                const balanceOfReceiver = await token.balanceOf(receiver);
+                
+                checkNumber(balanceOfDeployer, 900000);
+                checkNumber(balanceOfReceiver, 100000);
+            });
+            
+            it('emits a Transfer event', async () => {
+                const logs = result.logs[0];
+                expect(logs.event).to.equal('Transfer', 'event has been emitted');
+                expect(logs.args.from.toString()).to.eq(deployer, 'deployer is correct');
+                expect(logs.args.to.toString()).to.eq(receiver, 'receiver is correct');
+                logs.args.value.toString().should.eq(amount.toString(), 'amount is correct');
+            });
+        });
+        describe('failure', () => {
+            it('rejects insufficient funds', async () => {
+                let invalidAmount;
+                invalidAmount = tokens(100000000);
+                await token.transfer(receiver, invalidAmount, {from: deployer})
+                        .should.be.rejectedWith(EVM_REVERT);
+            });
+
+            // Attempts to send funds when you have none
+
+            it('rejects if balance is zero', async () => {
+                await token.transfer(deployer, amount, {from: receiver})
+                        .should.be.rejectedWith(EVM_REVERT);
+            });
+
+            it('rejects invalid recipients', async () => {
+                await token.transfer(0x0, amount, {from: deployer})
+                        .should.be.rejected;
+            });
+        });
     });
 });
+
