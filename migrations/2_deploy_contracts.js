386797ddbe1c61f0cd65b77ae6a09f8f21f320a3
Benoit Mrejen <benoit.mrejen@gmail.com>
2021-01-13 01:58:39 +0100

    add seed script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,15 @@
 // import Token from abis
-const Token = artifacts.require('Token');
+const Token = artifacts.require("Token");
+const Exchange = artifacts.require("Exchange");
 
-module.exports = function(deployer){
-    deployer.deploy(Token);
-}
+module.exports = async function(deployer) {
+  const accounts = await web3.eth.getAccounts();
+  await deployer.deploy(Token);
+
+  const feeAccount = accounts[0];
+  const feePercent = 10;
+  await deployer.deploy(Exchange, feeAccount, feePercent);
+};
+
+// deploy with > truffle migrate --reset (to launch a new copy of the contract on the blockchain)
+// then open Ganache --> Transactions to see the contracts deployed
